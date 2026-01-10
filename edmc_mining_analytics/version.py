4ce7d3464b5def3bdcbc035daa889ddeb2304372
SweetJonnySauce <sweetjonnysauce@gmail.com>
2026-01-10 09:59:03 -0800

    bumped version

--- edmc_mining_analytics/version.py
+++ edmc_mining_analytics/version.py
@@ -6,7 +6,7 @@ import re
 from typing import Iterable, Tuple
 
 
-PLUGIN_VERSION = "0.5.2"
+PLUGIN_VERSION = "0.5.3"
 VERSION: str = PLUGIN_VERSION
 __version__: str = PLUGIN_VERSION
 PLUGIN_REPO_URL = "https://github.com/SweetJonnySauce/EDMC-Mining-Analytics"
